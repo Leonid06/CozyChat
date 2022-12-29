668edc9b6546529424f5aadbed0fbeef157ddac0
Leonid06 <x.worklene@gmail.com>
2022-12-29 16:35:24 +0300

    initial commit

---
+++ chat/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+
+
+def main(request): 
+    return render(request, template_name= 'main.html')
+
+def login(request):
+    return 
+def logout(request): 
+    return 
