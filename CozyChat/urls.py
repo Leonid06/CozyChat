0ace8e8b2179735c8ef1d3ba0355bfe1160c9824
Leonid06 <x.worklene@gmail.com>
2022-12-29 18:10:47 +0300

    _

--- CozyChat/urls.py
+++ CozyChat/urls.py
@@ -1,11 +1,11 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from chat import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.main),
-    path('auth/login/', views.login),
-    path('auth/logout/', views.logout)
+    path('auth/register', views.register)
+    # path('auth/login', views.login)
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 
 
 def main(request): 
+    # user = User.objects.create_user()
     return render(request, template_name= 'main.html')
+
+
+def register(request):
+    # if request.method == "POST" :
+    #     form =
+    return 
 
 def login(request):
     return 
