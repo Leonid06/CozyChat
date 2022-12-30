f27026111ddd8ab91472580f09da36757510e52a
Leonid06 <x.worklene@gmail.com>
2022-12-30 14:25:20 +0300

    added socket script and socet logic itself

--- CozyChat/settings.py
+++ CozyChat/settings.py
@@ -27,6 +27,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer"
+    }
+}
+
 
 # Application definition
 
@@ -37,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'chat'
+    'chat',
+    'channels'
 ]
 
 MIDDLEWARE = [
@@ -69,6 +76,8 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'CozyChat.wsgi.application'
+
+ASGI_APPLICATION = 'CozyChat.asgi.application'
 
 
 # Database
@@ -117,6 +126,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
---
+++ chat/static/scripts/chat_script.js
@@ -0,0 +1,32 @@
+const chatSocket = new WebSocket("ws://" + window.location.host + "/") ;
+chatSocket.onopen = function(e){
+    console.log("connection was set up successfully")
+};
+chatSocket.onclose = function(e){
+    console.log("connection was closed");
+};
+
+let messageInputSelector = document.querySelector("#id_message_send_input");
+let messageButtonSelector = document.querySelector("#id_message_send_button");
+let chatItemContainer = document.querySelector("#id_chat_item_container")
+
+messageInputSelector.focus();
+
+messageInputSelector.onkeyup = function(e) {
+    if(e.keyCode == 13){
+        messageButtonSelector.click();
+    }
+};
+
+messageButtonSelector.onclick = function(e){
+    let messageInput = messageInputSelector.value;
+    chatSocket.send(JSON.stringify({message: messageInput, username: "{{username}}"}));
+};
+
+chatSocket.onmessage = function (e) {
+    const data = JSON.parse(e.data);
+    var div = document.createElement("div");
+    div.innerHTML = data.username + " : " + data.message;
+    messageInputSelector.value = "";
+    chatItemContainer.appendChild(div);
+}
